0da52f2331ecd892452c004d54cb212755b0139b
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-01-09 19:10:31 -0800

    Testing classes

---
+++ src/test/TestYeastCell.h
@@ -0,0 +1,31 @@
+#ifndef NEWTESTCLASS_H
+#define	NEWTESTCLASS_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "YeastCell.h"
+
+class TestYeastCell : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(TestYeastCell);
+
+    CPPUNIT_TEST(testMethod);
+    CPPUNIT_TEST(testVolume);
+    CPPUNIT_TEST(testFailedMethod);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    TestYeastCell();
+    virtual ~TestYeastCell();
+    void setUp();
+    void tearDown();
+
+private:
+    YeastCell *yc1, *yc2, *yc3;
+    void testMethod();
+    void testFailedMethod();
+    void testVolume();
+};
+
+#endif	/* NEWTESTCLASS_H */
+
