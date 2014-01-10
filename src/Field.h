d013a1e9524d7d0f0975f43c5fe64cbba8401194
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-01-09 19:09:14 -0800

    Field class

---
+++ src/Field.h
@@ -0,0 +1,28 @@
+#ifndef FIELD_H
+#define	FIELD_H
+
+class Field {
+public:
+    
+    Field();
+    Field(const Field& orig);
+    virtual ~Field();
+//    void set_dt(double dt) {this->dt = dt;};
+    
+    virtual bool is_static() {return(static_field); };
+    virtual void evolve() =0;
+    
+private:
+    bool static_field;
+    double x,y,z;
+    double dx,dy,dz;
+    int nodes_no;
+    int n_x, n_y, n_z;
+//    double grid [][][];
+//    double dt = 0.1;
+    
+    
+};
+
+#endif	/* FIELD_H */
+
