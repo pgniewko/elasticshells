3acfb50276dea544b7c54fface6d05b3d2399512
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-08-04 12:27:27 -0700

    Vertex class added to the project

---
+++ src/geometry/Vertex.cpp
@@ -0,0 +1,11 @@
+#include "Vertex.h"
+
+Vertex::Vertex() {
+}
+
+Vertex::Vertex(const Vertex& orig) {
+}
+
+Vertex::~Vertex() {
+}
+
---
+++ src/geometry/Vertex.h
@@ -0,0 +1,20 @@
+#ifndef VERTEX_H
+#define	VERTEX_H
+
+#include "Vector3D.h"
+#include "Point.h"
+
+class Vertex {
+public:
+    Vertex();
+    Vertex(const Vertex& orig);
+    virtual ~Vertex();
+private:
+//    Vector3D force;
+//    Vector3D velocity;
+    
+
+};
+
+#endif	/* VERTEX_H */
+
