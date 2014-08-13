bf9add8aea0e4ef43b1bb1c911bf5d66c3451382
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-08-13 02:15:58 -0700

    Framework for simulation

---
+++ src/simulation/Box.cpp
@@ -0,0 +1,49 @@
+#include "Box.h"
+
+Box::Box(double l_x, double l_y, double l_z, Vector3D o)
+{
+    this->set(l_x, l_y, l_z, o);
+}
+
+//Box::~Box()
+//{
+//}
+
+
+void Box::set(double l_x, double l_y, double l_z, Vector3D o)
+{
+    lx = l_x;
+    ly = l_y;
+    lz = l_z;
+    ilx = (lx ? 1.0 / lx : 0.0);
+    ily = (ly ? 1.0 / ly : 0.0);
+    ilz = (lz ? 1.0 / lz : 0.0);
+    O = o;
+}
+
+Vector3D Box::delta(const Vector3D& p1) const
+{
+    Vector3D delta = p1 - O;
+    delta.x -= lx * rint( ilx * delta.x );
+    delta.y -= ly * rint( ily * delta.y );
+    delta.z -= lz * rint( ilz * delta.z );
+    return delta;
+}
+
+Vector3D Box::delta(const Vector3D& p1, const  Vector3D& p2) const
+{
+    Vector3D delta = p1 - p2;
+    delta.x -= lx * rint( ilx * delta.x );
+    delta.y -= ly * rint( ily * delta.y );
+    delta.z -= lz * rint( ilz * delta.z );
+    return delta;
+}
+
+Vector3D Box::image(const Vector3D& p1) const
+{
+    Vector3D delta = p1 - O;
+    delta.x -= lx * rint( ilx * delta.x );
+    delta.y -= ly * rint( ily * delta.y );
+    delta.z -= lz * rint( ilz * delta.z );
+    return delta + O;
+}
---
+++ src/simulation/Simulator.h
@@ -0,0 +1,14 @@
+#ifndef SIMULATOR_H
+#define	SIMULATOR_H
+
+class Simulator {
+public:
+    Simulator();
+    Simulator(const Simulator& orig);
+    virtual ~Simulator();
+private:
+
+};
+
+#endif	/* SIMULATOR_H */
+
