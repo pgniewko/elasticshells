317539611e87616113c700f9dbeae9545bbe099f
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-08-29 10:38:15 -0700

    calcForce function moved to .cpp file

--- src/force/OsmoticForce.cpp
+++ src/force/OsmoticForce.cpp
@@ -5,3 +5,11 @@ OsmoticForce::OsmoticForce() {}
 OsmoticForce::OsmoticForce(const OsmoticForce& orig) {}
 
 OsmoticForce::~OsmoticForce() {}
+
+static Vector3D OsmoticForce::calcForce(const Vector3D& va, const Vector3D& vb, const Vector3D& vc, const Vector3D& vd, const double dp)
+{
+        Vector3D BD = vb - vd;
+        Vector3D CD = vc - vd;
+        Vector3D f = -dp * cross(BD, CD) / 6;
+        return f;
+}
--- src/force/OsmoticForce.h
+++ src/force/OsmoticForce.h
@@ -9,13 +9,7 @@ public:
     OsmoticForce();
     OsmoticForce(const OsmoticForce& orig);
     virtual ~OsmoticForce();
-    static Vector3D calcForce(const Vector3D& va, const Vector3D& vb, const Vector3D& vc, const Vector3D& vd, const double dp)
-    {
-        Vector3D BD = vb - vd;
-        Vector3D CD = vc - vd;
-        Vector3D f = -dp * cross(BD, CD) / 6;
-        return f;
-    }
+    static Vector3D calcForce(const Vector3D&, const Vector3D&, const Vector3D&, const Vector3D&, const double);
 private:
 
 };
