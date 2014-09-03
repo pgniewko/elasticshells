c73c91bbc7b4dd3009b75c4934111077ef0d95f1
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-09-03 11:12:46 -0700

    New exceptions: data and not implemented exceptions

---
+++ src/exceptions/DataException.h
@@ -0,0 +1,15 @@
+#ifndef DATAEXCEPTION_H
+#define	DATAEXCEPTION_H
+
+#include <string>
+#include <stdexcept>
+using namespace std;
+
+class DataException : public std::runtime_error
+{
+public:
+  DataException(const string& what_arg) : runtime_error( what_arg ) {}
+};
+
+#endif	/* DATAEXCEPTION_H */
+
---
+++ src/exceptions/NotImplementedException.h
@@ -0,0 +1,15 @@
+#ifndef NOTIMPLEMENTEDEXCEPTION_H
+#define	NOTIMPLEMENTEDEXCEPTION_H
+
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class NotImplementedException : public std::runtime_error
+{
+public:
+  NotImplementedException(const string& what_arg) : runtime_error( what_arg ) {}
+};
+
+#endif	/* NOTIMPLEMENTEDEXCEPTION_H */
+
