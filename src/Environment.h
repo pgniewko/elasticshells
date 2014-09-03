7c100de513f1a6dbf5641b77ba418e47e8185f6b
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-09-03 14:04:49 -0700

    Environment variables header

---
+++ src/Environment.h
@@ -0,0 +1,20 @@
+#ifndef ENVIRONMENT_H
+#define	ENVIRONMENT_H
+
+#define STRCMP(a,b) (!strcmp(a,b))
+
+#define MAX_CELLS 100
+#define MAX_V 200
+#define MAX_T 500
+#define NEIGH_MAX 10
+#define TRIAN_MAX 10
+
+const double E     = 2.71828182845905;
+const double PI    = 3.14159265358979;
+const double SQRT2 = 1.41421356237310;
+const double SQRT3 = 1.73205080756888;
+const double DELTA = 0.00000000000001;
+
+
+#endif	/* ENVIRONMENT_H */
+
