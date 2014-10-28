f9f0db0d6daf814fa4a908af3f01c357b77be739
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-10-28 14:48:00 -0700

    Box class fields - bugfix

--- src/simulation/Box.cpp
+++ src/simulation/Box.cpp
@@ -1,14 +1,19 @@
 #include "Box.h"
 
-Box::Box(double bsx, double bsy, double bsz) : x(bsx), y(bsy), z(bsz),
-    xe(0.5 * bsx), ye(0.5 * bsy), ze(0.5 * bsz), dx(0), dy(0), dz(0), pbc(false), ecw(0.0) {}
+Box::Box(double bsx, double bsy, double bsz) : pbc(false), ecw(0.0), 
+        x(bsx), y(bsy), z(bsz),
+        xe(0.5 * bsx), ye(0.5 * bsy), ze(0.5 * bsz), 
+        dx(0), dy(0), dz(0){}
 
-Box::Box(double bsx, double bsy, double bsz, double dbs) : x(bsx), y(bsy), z(bsz),
-    xe(0.5 * bsx), ye(0.5 * bsy), ze(0.5 * bsz), dx(dbs), dy(dbs), dz(dbs), pbc(false), ecw(0.0) {}
+Box::Box(double bsx, double bsy, double bsz, double dbs) : pbc(false), ecw(0.0), 
+        x(bsx), y(bsy), z(bsz),
+        xe(0.5 * bsx), ye(0.5 * bsy), ze(0.5 * bsz), 
+        dx(dbs), dy(dbs), dz(dbs){}
 
-Box::Box(const Box& orig) : x(orig.x), y(orig.y), z(orig.z),
-    xe(orig.xe), ye(orig.ye), ze(orig.ze), dx(orig.dx), dy(orig.dy), dz(orig.dz),
-    pbc(orig.pbc), ecw(orig.ecw) {}
+Box::Box(const Box& orig) : pbc(orig.pbc), ecw(orig.ecw),
+        x(orig.x), y(orig.y), z(orig.z),
+        xe(orig.xe), ye(orig.ye), ze(orig.ze), 
+        dx(orig.dx), dy(orig.dy), dz(orig.dz){}
 
 Box::~Box() {}
 
