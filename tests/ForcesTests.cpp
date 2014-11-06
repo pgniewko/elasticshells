73982c9683e5f66faced7126599aae529d4fb247
Pawel Gniewek <gniewko.pablo@gmail.com>
2014-11-05 16:21:22 -0800

    ForcesTests framework

---
+++ tests/ForcesTests.cpp
@@ -0,0 +1,25 @@
+#include "ForcesTests.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ForcesTests);
+
+ForcesTests::ForcesTests() {
+}
+
+ForcesTests::~ForcesTests() {
+}
+
+void ForcesTests::setUp() {
+
+}
+
+void ForcesTests::tearDown() {
+
+}
+
+void ForcesTests::testMethod() {
+    CPPUNIT_ASSERT(true);
+}
+
+void ForcesTests::testFailedMethod() {
+    CPPUNIT_ASSERT(false);
+}
---
+++ tests/ForcesTests.h
@@ -0,0 +1,36 @@
+#ifndef FORCESTESTS_H
+#define	FORCESTESTS_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "Environment.h"
+#include "force/HertzianRepulsion.h"
+#include "force/HookeanForce.h"
+#include "force/NbRepulsiveForce.h"
+#include "force/OsmoticForce.h"
+#include "geometry/Vector3D.h"
+#include "geometry/Vertex.h"
+#include "geometry/Tetrahedron.h"
+
+class ForcesTests : public CPPUNIT_NS::TestFixture 
+{
+    CPPUNIT_TEST_SUITE(ForcesTests);
+    
+    CPPUNIT_TEST(testMethod);
+
+    CPPUNIT_TEST_FAIL(testFailedMethod);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    ForcesTests();
+    virtual ~ForcesTests();
+    void setUp();
+    void tearDown();
+
+private:
+    void testMethod();
+    void testFailedMethod();
+};
+
+#endif	/* FORCESTESTS_H */
