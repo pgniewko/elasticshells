baa6322e5453b515a38f230e0994bcda4c6e2fbd
Pawel Gniewek <gniewko.pablo@gmail.com>
2015-01-01 19:53:16 +0100

    Paddning bugfix

--- src/utils/io/LogSimulation.cpp
+++ src/utils/io/LogSimulation.cpp
@@ -26,7 +26,7 @@ void LogSimulation::dumpState(Box& box, std::vector<Cell>& cells, double rv, int
     double cellsVolume = VolumeFraction::caclCellsVolume(cells);
     double boxVolume = box.getVolume(rv);
     int numofcells = cells.size();
-    fprintf(os, "%i %i %i %8.2f %8.2f %10.8f %8.6f\n", simstep, numofcells, numV, boxVolume, cellsVolume, volumeFrac, pressure);
-    //double q6 = QL::calcQl(cells[0], 4, 5.0);
-    //fprintf(os, "%i %i %i %8.2f %8.2f %10.8f %8.6f %8.6f\n", simstep, numofcells, numV, boxVolume, cellsVolume, volumeFrac, pressure, q6);
+    //fprintf(os, "%i %i %i %8.2f %8.2f %10.8f %8.6f\n", simstep, numofcells, numV, boxVolume, cellsVolume, volumeFrac, pressure);
+    double q6 = QL::calcQl( cells, 6, box.getX() );
+    fprintf(os, "%9i %3i %5i %8.2f %8.2f %10.8f %8.6f %8.6f\n", simstep, numofcells, numV, boxVolume, cellsVolume, volumeFrac, pressure, q6);
 }
