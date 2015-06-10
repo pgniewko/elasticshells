d18e97deced675849d21d1709bc97bee3b5c2bb8
Pawel Gniewek <gniewko.pablo@gmail.com>
2015-06-10 14:19:25 -0700

    New observable classe: TotalCellsArea

---
+++ src/utils/observables/TotalCellsArea.cpp
@@ -0,0 +1,17 @@
+#include "TotalCellsArea.h"
+
+TotalCellsArea::TotalCellsArea() {}
+
+TotalCellsArea::TotalCellsArea(const TotalCellsArea& orig) {}
+
+TotalCellsArea::~TotalCellsArea() {}
+
+double TotalCellsArea::totalCellArea(std::vector<Cell>& cells)
+{
+    int cellsnumber = cells.size();
+    double total_area = 0.0;
+    for (int i = 0; i < cellsnumber; i++)
+    {
+        total_area += cells[i].calcSurfaceArea();
+    }
+}
---
+++ src/utils/observables/TotalCellsArea.h
@@ -0,0 +1,17 @@
+#ifndef TOTALCELLSAREA_H
+#define	TOTALCELLSAREA_H
+
+#include <vector>
+#include "Cell.h"
+
+class TotalCellsArea {
+public:
+    TotalCellsArea();
+    TotalCellsArea(const TotalCellsArea& orig);
+    virtual ~TotalCellsArea();
+    static double totalCellArea(std::vector<Cell>&);
+private:
+
+};
+
+#endif	/* TOTALCELLSAREA_H */
