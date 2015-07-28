d64b5a120e037d927817e2fedf3a720721375f48
Pawel Gniewek <gniewko.pablo@gmail.com>
2015-07-27 17:51:15 -0700

    VolumeFraction tailored to the new Observer

--- src/utils/observables/VolumeFraction.cpp
+++ src/utils/observables/VolumeFraction.cpp
@@ -1,20 +1,21 @@
 #include "VolumeFraction.h"
 
-VolumeFraction::VolumeFraction() {}
+VolumeFraction::VolumeFraction(const char* name, const char* format) : Observer(name, format)
+{}
 
-VolumeFraction::VolumeFraction(const VolumeFraction& orig) {}
+VolumeFraction::VolumeFraction(const VolumeFraction& orig) : Observer(orig.observer_name, orig.output_format), rv(orig.rv)
+{}
 
 VolumeFraction::~VolumeFraction() {}
 
-double VolumeFraction::calcVolumeFraction(Box& box, std::vector<Cell>& cells, double rv)
+double VolumeFraction::observe(Box& box, std::vector<Cell>& cells)
 {
-    //double boxVolume = box.getVolume(rv);
     double boxVolume = box.getVolume(0.0);
-    double cellsVolume = calcCellsVolume(cells, rv);
+    double cellsVolume = calcCellsVolume(cells);
     return (cellsVolume / boxVolume);
 }
 
-double VolumeFraction::calcCellsVolume(std::vector<Cell>& cells, double rv)
+double VolumeFraction::calcCellsVolume(std::vector<Cell>& cells)
 {
     double cellsVolume = 0.0;
     int numofcells = cells.size();
@@ -26,3 +27,18 @@ double VolumeFraction::calcCellsVolume(std::vector<Cell>& cells, double rv)
 
     return cellsVolume;
 }
+
+void VolumeFraction::set_params(int num, ...)
+{
+    va_list arguments;
+    va_start (arguments, num);
+    rv = va_arg(arguments, double);
+    va_end( arguments );
+}
+
+void VolumeFraction::set_params(int num, std::vector<std::string> args_)
+{
+    rv = strtod(args_[ num+0 ].c_str(), NULL);
+}
+
+DerivedRegister<VolumeFraction> VolumeFraction::reg("VolumeFraction");
--- src/utils/observables/VolumeFraction.h
+++ src/utils/observables/VolumeFraction.h
@@ -1,21 +1,25 @@
 #ifndef VOLUMEFRACTION_H
 #define	VOLUMEFRACTION_H
 
-#include <vector>
+#include "utils/observables/Observer.h"
 
-#include "simulation/Box.h"
-#include "Cell.h"
-
-class VolumeFraction
+class VolumeFraction : public Observer
 {
-    public:
-        VolumeFraction();
+public:
+        VolumeFraction(const char*, const char*);
         VolumeFraction(const VolumeFraction& orig);
         virtual ~VolumeFraction();
-        static double calcVolumeFraction(Box&, std::vector<Cell>&, double = 0.0);
-        static double calcCellsVolume(std::vector<Cell>&, double = 0.0);
-    private:
-
+        
+        void set_params(int, ...);
+        void set_params(int, std::vector<std::string>);
+        double observe(Box&, std::vector<Cell>&);
+        
+private:
+    double calcVolumeFraction(Box&, std::vector<Cell>&);
+    double calcCellsVolume(std::vector<Cell>&);
+    
+    static DerivedRegister<VolumeFraction> reg;
+    double rv;
 };
 
 #endif	/* VOLUMEFRACTION_H */
