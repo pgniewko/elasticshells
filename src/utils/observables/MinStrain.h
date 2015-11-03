37ed5852992130f65feb0569e5277f58050a83c1
Pawel Gniewek <gniewko.pablo@gmail.com>
2015-11-03 14:37:00 -0800

    New class: MinStrain

---
+++ src/utils/observables/MinStrain.h
@@ -0,0 +1,22 @@
+#ifndef MINSTRAIN_H
+#define	MINSTRAIN_H
+
+#include "utils/observables/Observer.h"
+
+class MinStrain : public Observer
+{
+    public:
+        MinStrain(const char*, const char*);
+        MinStrain(const MinStrain& orig);
+        virtual ~MinStrain();
+        
+        void set_params(const int, std::vector<std::string>);
+        double observe(const Box&, std::vector<Cell>&);
+        
+    private:
+        static DerivedRegister<MinStrain> reg;
+
+};
+
+#endif	/* MINSTRAIN_H */
+
