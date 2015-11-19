a68829d317de34d60b20714b823bd8c0def646bf
Pawel Gniewek <gniewko.pablo@gmail.com>
2015-11-19 15:58:41 -0800

    New observable class: AverageActiveArea

---
+++ src/utils/observables/AverageActiveArea.h
@@ -0,0 +1,22 @@
+#ifndef AVERAGEACTIVEAREA_H
+#define	AVERAGEACTIVEAREA_H
+
+#include "utils/observables/Observer.h"
+
+class AverageActiveArea : public Observer
+{
+public:
+    AverageActiveArea(const char*, const char*);
+    AverageActiveArea(const AverageActiveArea& orig);
+    virtual ~AverageActiveArea();
+    
+    void set_params(const int, std::vector<std::string>);
+    double observe(const Box&, std::vector<Cell>&);
+    
+private:
+    static DerivedRegister<AverageActiveArea> reg;
+
+};
+
+#endif	/* AVERAGEACTIVEAREA_H */
+
