6def97195a98c5be077b62bbc70a6193e585f4db
Pawel Gniewek <gniewko.pablo@gmail.com>
2016-03-08 23:23:48 -0800

    BoxSizeObserver class added

---
+++ src/utils/observables/BoxSizeObserver.cpp
@@ -0,0 +1,38 @@
+#include "BoxSizeObserver.h"
+
+BoxSizeObserver::BoxSizeObserver(const char* name, const char* format) : Observer(name, format) {}
+
+BoxSizeObserver::BoxSizeObserver(const BoxSizeObserver& orig) : Observer(orig) {}
+
+BoxSizeObserver::~BoxSizeObserver() {}
+
+void BoxSizeObserver::set_params(const int num, std::vector<std::string> args_)
+{
+    i_param = atoi(args_[ num + 0 ].c_str());
+};
+
+double BoxSizeObserver::observe(const Box& box, std::vector<Cell>& cells)
+{
+    switch(i_param)
+    {
+        case 1:
+            return box.getX();
+            //break;
+            
+        case 2:
+            return box.getY();
+            //break;
+           
+        case 3:
+            return box.getZ();
+            //break;
+            
+        default:
+            return 0.0;
+            //break;
+    }
+    
+    return 0.0;
+}
+
+DerivedRegister<BoxSizeObserver> BoxSizeObserver::reg("BoxSizeObserver");
---
+++ src/utils/observables/BoxSizeObserver.h
@@ -0,0 +1,22 @@
+#ifndef BOXSIZEOBSERVER_H
+#define	BOXSIZEOBSERVER_H
+
+#include "utils/observables/Observer.h"
+
+class BoxSizeObserver : public Observer
+{
+    public:
+        BoxSizeObserver(const char*, const char*);
+        BoxSizeObserver(const BoxSizeObserver& orig);
+        virtual ~BoxSizeObserver();
+
+        double observe(const Box&, std::vector<Cell>&);
+        void set_params(const int, std::vector<std::string>);
+
+    private:
+        static DerivedRegister<BoxSizeObserver> reg;
+
+};
+
+#endif	/* BOXSIZEOBSERVER_H */
+
