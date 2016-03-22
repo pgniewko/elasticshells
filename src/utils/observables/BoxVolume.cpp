1e2d5d235b791d4217f5509da78adb49363b1583
Pawel Gniewek <gniewko.pablo@gmail.com>
2016-03-21 18:31:52 -0700

    BoxVolume observable class added

---
+++ src/utils/observables/BoxVolume.cpp
@@ -0,0 +1,20 @@
+#include "BoxVolume.h"
+
+BoxVolume::BoxVolume (const char* name, const char* format) : Observer(name, format) {}
+
+BoxVolume::BoxVolume(const BoxVolume& orig) : Observer(orig) {}
+
+BoxVolume::~BoxVolume() {}
+
+void BoxVolume::set_params(const int num, std::vector<std::string> args_)
+{
+    d_param = strtod(args_[ num + 0 ].c_str(), NULL);
+}
+
+double BoxVolume::observe(const Box& box, std::vector<Cell>& cells)
+{
+    return box.getVolume(d_param);
+}
+
+DerivedRegister<BoxVolume> BoxVolume::reg("BoxVolume");
+
