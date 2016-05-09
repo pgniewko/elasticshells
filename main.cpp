e785cb92976ceb4e89e62c78dec78c30370003ce
Pawel Gniewek <gniewko.pablo@gmail.com>
2016-05-09 11:08:34 -0700

    Removed redundant code

--- main.cpp
+++ main.cpp
@@ -483,6 +483,7 @@ int main(int argc, char** argv)
     biofilm_logs << utils::LogLevel::FILE << "STRESS_FILE = "      << arguments.stress_file << "\n";
     biofilm_logs << utils::LogLevel::FILE << "OBSERVERS_CONFIG = " << arguments.ob_config_file << "\n";
 
+    //Cell::membrane_test = true;
     clocks[0].tic();
     simulation_time = read_timer();
     Simulator simulator(arguments);
@@ -490,22 +491,7 @@ int main(int argc, char** argv)
     simulator.simulate(arguments.nsteps);
     clocks[0].toc();
     simulation_time = read_timer( ) - simulation_time;
-
-    MembraneTriangulation mt;
-    //mt.triangulate(2.0, 0.5, 15);
-    //mt.saveTriangulatedSurface("membrane.xyz", false);
-    //mt.saveRenderingScript("membrane.py", "membrane.xyz");
     
-    Cell test_cell(mt.triangulate(1.0,0.5, 15));
-    std::vector<Cell> cells;
-    cells.push_back(test_cell);
-    ScriptBuilder sb("render.py","surface.py","traj.xyz","stress.py");
-    Box box(0,0,0);
-    sb.saveRenderScript(cells, box, false, 0.05);
-    
-    XyzTraj traj("traj.xyz","box.xyz");
-    traj.open();
-    traj.save(cells, cells[0].getNumberVertices(), 1,1,1);
     
 #ifdef _OPENMP
     int gt = omp_get_max_threads();
