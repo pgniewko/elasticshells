3cb8a277744d26c2a40ef809fe6419ad6684756d
Pawel Gniewek <gniewko.pablo@gmail.com>
2016-07-20 16:13:11 -0700

    Energy class added to the project

---
+++ src/simulation/Energy.cpp
@@ -0,0 +1,11 @@
+#include "Energy.h"
+
+Energy::Energy() {
+}
+
+Energy::Energy(const Energy& orig) {
+}
+
+Energy::~Energy() {
+}
+
---
+++ src/simulation/Energy.h
@@ -0,0 +1,14 @@
+#ifndef ENERGY_H
+#define	ENERGY_H
+
+class Energy {
+public:
+    Energy();
+    Energy(const Energy& orig);
+    virtual ~Energy();
+private:
+
+};
+
+#endif	/* ENERGY_H */
+
