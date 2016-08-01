8a7a5ffb1ce82ba88d414be2120648b58037e797
Pawel Gniewek <gniewko.pablo@gmail.com>
2016-08-01 13:41:22 -0700

    Improvement to ActiveArea and ActiveFraction classes

--- src/Cell.h
+++ src/Cell.h
@@ -88,10 +88,17 @@ class Cell
         double contactForce(const Cell&, const Box&) const;
         double contactForce(const Box&) const;
         double contactForceSF(const Box&) const; // for Surface Force use
+        
         double contactArea(const Cell&, const Box&) const;
+        double contactArea(const Cell&, const Box&, const double) const;
         double contactArea(const Box&, double = 0.0) const;
-        double activeArea(const Box&, const std::vector<Cell>&, double&, bool = false) const;
-        double activeAreaFraction(const Box&, const std::vector<Cell>&, double&, bool = false) const;
+        
+        
+        double activeArea(const Box&, const std::vector<Cell>&, double = 0.0) const;
+        double calcSurfaceArea(double) const;
+        double contactArea2(const Box&, double = 0.0) const;
+        //double activeAreaFraction(const Box&, const std::vector<Cell>&, double&, double = 0.0) const;
+        
         double strainEnergy(const Box&) const;
         double maxStrain() const;
         double minStrain() const;
