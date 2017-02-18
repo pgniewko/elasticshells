e9962fecfc55e4189026328d8543c04a4b76f0e7
Pawel Gniewek <pawel.gniewek@berkeley.edu>
2017-02-17 17:31:25 -0800

    CvTurgor class

---
+++ src/utils/observables/CvTurgor.cpp
@@ -0,0 +1,34 @@
+#include "CvTurgor.h"
+
+CvTurgor::CvTurgor(const char* name, const char* format) : Observer(name, format) {}
+
+CvTurgor::CvTurgor(const CvTurgor& orig)  : Observer(orig) {}
+
+CvTurgor::~CvTurgor() {}
+
+void CvTurgor::set_params(const int num, std::vector<std::string> args_)
+{
+    return;
+};
+
+double CvTurgor::observe(const Box& box, std::vector<Cell>& cells)
+{
+    int N = cells.size();
+    double av_turgor = 0.0;
+    double av_turgor2 = 0.0;
+
+    for (int i = 0; i < N; i++)
+    {
+        av_turgor  += cells[i].getTurgor();
+        av_turgor2 += cells[i].getTurgor() * cells[i].getTurgor();
+    }
+
+    av_turgor /= N;
+    av_turgor2 /= N;
+    
+    double stdev = sqrt( av_turgor2 - av_turgor*av_turgor );
+    double cv = stdev / av_turgor;
+    return cv;
+}
+
+DerivedRegister<CvTurgor> CvTurgor::reg("CvTurgor");
