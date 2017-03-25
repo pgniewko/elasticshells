96c3ad46a6346e4961f96fb67683294ef58dd128
Pawel Gniewek <pawel.gniewek@berkeley.edu>
2017-03-24 17:32:30 -0700

    Proper implementation of ShapeQl and ShapeWl

--- src/utils/observables/order/ShapeWl.cpp
+++ src/utils/observables/order/ShapeWl.cpp
@@ -9,53 +9,55 @@ ShapeWl::~ShapeWl() {}
 void ShapeWl::set_params(const int num, std::vector<std::string> args_)
 {
     i_param = atoi(args_[ num + 0 ].c_str());
-    d_param = strtod(args_[ num + 1 ].c_str(), NULL);
 }
 
 double ShapeWl::observe(const Box& box, std::vector<Cell>& cells)
 {
     double wlsum = 0.0;
-    double N = 0.0;
 
     for (uint i = 0; i < cells.size(); i++)
     {
         wlsum += ShapeWl::calcWl(cells[i]);
-        N += 1.0;
     }
 
-    wlsum /= N;
+    wlsum /= (double) cells.size();
     return wlsum;
 }
 
 double ShapeWl::calcWl(Cell& cell)
 {
-    int count;
-    int n = cell.getNumberVertices();
+    int nk = cell.getNumberVertices();
+    
     double qss = 0.0;
     double* x, *y, *z;
     double* qlRe, *qlIm;
-    x = (double*) malloc (n * sizeof (double));
-    y = (double*) malloc (n * sizeof (double));
-    z = (double*) malloc (n * sizeof (double));
+    x = (double*) malloc (nk * sizeof (double));
+    y = (double*) malloc (nk * sizeof (double));
+    z = (double*) malloc (nk * sizeof (double));
 
-    for (int i = 0; i < n ; i++)
+    for (int i = 0; i < nk ; i++)
     {
         x[i] = cell.vertices[i].r_c.x;
         y[i] = cell.vertices[i].r_c.y;
         z[i] = cell.vertices[i].r_c.z;
     }
+    
+    Vector3D cm = cell.getCm();
+    double xc = cm.x;
+    double yc = cm.y;
+    double zc = cm.z;
 
     qlRe = (double*) malloc ((i_param + 1) * sizeof (double));
     qlIm = (double*) malloc ((i_param + 1) * sizeof (double));
-    double r_cutoff = cell.getInitR() * d_param;
-    count = qlm (i_param, n, r_cutoff, x, y, z, qlRe, qlIm);
+    
+    qlm (i_param, nk, xc, yc, zc, x, y, z, qlRe, qlIm);
     qss = qsum (i_param, qlRe, qlIm);
 
     double wlval = 0.0;
 
     if (qss > 1e-3)
     {
-        wlval = wl (i_param, qlRe, qlIm) / (qss * qss * qss);
+        wlval = Wl (i_param, qlRe, qlIm) / (qss * qss * qss);
     }
 
     free (qlRe);
