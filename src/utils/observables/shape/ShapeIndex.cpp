98ef64be46ebe79127635e2d5c9e4ca8c74ae300
Pawel Gniewek <pawel.gniewek@berkeley.edu>
2017-10-23 16:21:14 -0700

    Shape index added to Observers

---
+++ src/utils/observables/shape/ShapeIndex.cpp
@@ -0,0 +1,31 @@
+#include "ShapeIndex.h"
+
+ShapeIndex::ShapeIndex(const char* name, const char* format) : Observer(name, format) {}
+
+ShapeIndex::ShapeIndex(const ShapeIndex& orig) : Observer(orig) {}
+
+ShapeIndex::~ShapeIndex() {}
+
+void ShapeIndex::set_params(const int num, std::vector<std::string> args_)
+{
+    return;
+};
+
+double ShapeIndex::observe(const Box& box, std::vector<Cell>& cells, const DomainList& dl)
+{
+    double q = 0.0;
+    
+    double a, v;
+    
+    for (uint i = 0; i < cells.size(); i++)
+    {
+        a = cells[i].calcSurfaceArea(0.0);
+        v = cells[i].calcVolume(0.0);;
+        q += ( a / pow(v, 2.0/3.0) ) ; 
+    }
+    
+    q /= cells.size();
+    return q;
+}
+
+DerivedRegister<ShapeIndex> ShapeIndex::reg("ShapeIndex");
---
+++ src/utils/observables/shape/ShapeIndex.h
@@ -0,0 +1,22 @@
+#ifndef SHAPEINDEX_H
+#define SHAPEINDEX_H
+
+#include "utils/observables/Observer.h"
+
+class ShapeIndex  : public Observer {
+public:
+    public:
+        explicit ShapeIndex(const char*, const char*);
+        ShapeIndex(const ShapeIndex& orig);
+        virtual ~ShapeIndex();
+
+        void set_params(const int, std::vector<std::string>);
+        double observe(const Box&, std::vector<Cell>&, const DomainList&);
+
+    private:
+        static DerivedRegister<ShapeIndex> reg;
+
+};
+
+#endif /* SHAPEINDEX_H */
+
