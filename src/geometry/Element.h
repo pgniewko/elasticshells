d539697887aa0b8bc28886eee1b83b69c3560307
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-11-21 20:47:52 -0800

    New structures

---
+++ src/geometry/Element.h
@@ -0,0 +1,15 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+struct element
+{
+    int ia, ib, ic;
+    double an[3];
+    double L2[3];
+    double ki[3];
+    double ci[3];
+};
+
+
+#endif /* ELEMENT_H */
+
---
+++ src/geometry/Hinge.h
@@ -0,0 +1,15 @@
+#ifndef HINGE_H
+#define HINGE_H
+
+
+struct hinge
+{
+    int x1, x2, x3, x4;
+    double D;
+    double sinTheta0;
+    double theta0;
+};
+
+
+#endif /* HINGE_H */
+
