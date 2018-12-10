7f6a438582eb8c8fe4e2a3b14c8aab9a0ae244d8
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-12-10 12:36:55 -0800

    Adding BoxVolume obseerver

---
+++ src/utils/observables/BoxVolume.h
@@ -0,0 +1,22 @@
+#ifndef BOXVOLUME_H
+#define	BOXVOLUME_H
+
+#include "utils/observables/Observer.h"
+
+class BoxVolume : public Observer
+{
+    public:
+    public:
+        explicit BoxVolume(const char*, const char*);
+        BoxVolume(const BoxVolume& orig);
+        virtual ~BoxVolume();
+
+        void set_params(const int, std::vector<std::string>);
+        double observe(const Box&, const std::vector<Shell>&);
+
+    private:
+        static DerivedRegister<BoxVolume> reg;
+};
+
+#endif	/* BOXVOLUME_H */
+
