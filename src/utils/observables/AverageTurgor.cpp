dac635bdaa3bfa769137ad7856ce02a8b676bc11
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-12-10 12:36:11 -0800

    Addint AverageTurgor observer

---
+++ src/utils/observables/AverageTurgor.cpp
@@ -0,0 +1,28 @@
+#include "AverageTurgor.h"
+
+AverageTurgor::AverageTurgor(const char* name, const char* format) : Observer(name, format) {}
+
+AverageTurgor::AverageTurgor(const AverageTurgor& orig) : Observer(orig) {}
+
+AverageTurgor::~AverageTurgor() {}
+
+void AverageTurgor::set_params(const int num, std::vector<std::string> args_)
+{
+    return;
+};
+
+double AverageTurgor::observe(const Box& box, const std::vector<Shell>& shells)
+{
+    uint N = shells.size();
+    double av_turgor = 0.0;
+
+    for (uint i = 0; i < N; i++)
+    {
+        av_turgor += shells[i].get_turgor();
+    }
+
+    av_turgor /= N;
+    return av_turgor;
+}
+
+DerivedRegister<AverageTurgor> AverageTurgor::reg("AverageTurgor");
---
+++ src/utils/observables/AverageTurgor.h
@@ -0,0 +1,20 @@
+#ifndef AVERAGETURGOR_H
+#define	AVERAGETURGOR_H
+
+#include "utils/observables/Observer.h"
+
+class AverageTurgor : public Observer
+{
+    public:
+        explicit AverageTurgor(const char*, const char*);
+        AverageTurgor(const AverageTurgor& orig);
+        virtual ~AverageTurgor();
+
+        void set_params(const int, std::vector<std::string>);
+        double observe(const Box&, const std::vector<Shell>&);
+
+    private:
+        static DerivedRegister<AverageTurgor> reg;
+};
+
+#endif	/* AVERAGETURGOR_H */
