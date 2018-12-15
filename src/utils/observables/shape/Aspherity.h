4de95aeda635ee922017e21b9b206686d5457c5c
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-12-14 16:15:17 -0800

    Aspherity class added

---
+++ src/utils/observables/shape/Aspherity.h
@@ -0,0 +1,22 @@
+#ifndef ASPHERITY_H
+#define	ASPHERITY_H
+
+#include "geometry/Vector3D.h"
+#include "utils/observables/Observer.h"
+
+class Aspherity : public Observer
+{
+    public:
+        explicit Aspherity(const char*, const char*);
+        Aspherity(const Aspherity& orig);
+        virtual ~Aspherity();
+
+        void set_params(const int, std::vector<std::string>);
+        double observe(const Box&, const std::vector<Shell>&);
+
+    private:
+        static DerivedRegister<Aspherity> reg;
+};
+
+#endif	/* ASPHERITY_H */
+
